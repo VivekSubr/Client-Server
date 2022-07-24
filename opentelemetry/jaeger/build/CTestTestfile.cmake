7c940dfcebba55fbc2d6f4a070c2421d5ae21581
VivekSubr <tzar123@gmail.com>
2022-07-24 12:30:44 +0530

    cleanup

--- opentelemetry/jaeger/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vivek/Client-Server/opentracing/jaeger
-# Build directory: /home/vivek/Client-Server/opentracing/jaeger/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TracerTest "/home/vivek/Client-Server/opentracing/jaeger/build/TracerTest")
-set_tests_properties(TracerTest PROPERTIES  _BACKTRACE_TRIPLES "/home/vivek/Client-Server/opentracing/jaeger/CMakeLists.txt;36;add_test;/home/vivek/Client-Server/opentracing/jaeger/CMakeLists.txt;0;")
