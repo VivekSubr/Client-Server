9725a46f7f53b19eba1c971f0c1ca3fb4aab65f3
VivekSubr <tzar123@gmail.com>
2023-06-04 14:45:15 +0530

    added names

--- opentelemetry/prometheus/run.sh
+++ opentelemetry/prometheus/run.sh
@@ -2,14 +2,14 @@ set -x
 
 rm -f otel.log prom.log
 pkill prometheus
-pkill otelcol
+pkill otel
 
 #https://opentelemetry.io/docs/collector/getting-started/
-docker run -p 4317:4317 \
+docker run --name otel -p 4317:4317 \
     -v $(pwd)/otel_config.yaml:/etc/otelcol/config.yaml \
     otel/opentelemetry-collector:0.71.0 &> otel.log & 
 
-docker run \
+docker run --name prometheus \
     -p 9090:9090 \
     -v $(pwd)/prometheus.yaml:/etc/prometheus/prometheus.yml \
     prom/prometheus &> prom.log &
